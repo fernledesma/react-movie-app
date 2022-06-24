13d23cb4d3a1123fa27d0468d18ef9d8e45f83e1
Jose Fernando Ledesma <fernledesma@gmail.com>
2022-06-23 18:25:58 -0700

    Final Commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import LandingPage from "./features/MovieSearch/pages/LandingPage.jsx";
 import { MoviesProvider } from "./features/Providers/MoviesProvider";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import FavoritesPage from './features/FavoriteMovies/pages/FavoritesPage';
 import Navbar from "./features/Navbar/Navbar";
 
@@ -14,8 +14,9 @@ root.render(
       <MoviesProvider>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/react-movie-app" component={LandingPage} />
           <Route path="/favorites" component={FavoritesPage} />
+          <Redirect to="/react-movie-app"/>
         </Switch>
       </MoviesProvider>
     </BrowserRouter>
